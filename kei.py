4d2a534c9ffe964793874e59b987e678d7c8e0b5
dev-xiaome <dev-xiaome@outlook.com>
2026-04-11 23:30:13 +0800

    1.8-6

--- kei.py
+++ kei.py
@@ -12,7 +12,7 @@ import os
 if __name__ == '__main__':
     sys.modules['kei'] = sys.modules['__main__']
 
-__version__ = "1.8-5"
+__version__ = "1.8-6"
 
 class KeiState:
     stack: List[Any]
@@ -264,8 +264,8 @@ def error(errtype: str | None, info: str, stack: list=[], code:str|None=None, li
 
     print(f"{space} ·")
 
-    import traceback
-    traceback.print_exc()
+    #import traceback
+    #traceback.print_exc()
 
     if not __kei__.repl:
         sys.exit(1)
---
+++ lib/braces.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+
+print("We share the same ideals!👍")
